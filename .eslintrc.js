232d25a82cdb25521692f2a3809ea1e7f10aa145
fayzzzm <m.fayzov1@gmail.com>
2020-04-24 20:06:58 +0500

    Added linked list, deep dive to keys and deep copy

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,5 +19,10 @@ module.exports = {
     'no-bitwise': 0,
     'import/no-unresolved': 0,
     'implicit-arrow-linebreak': 0,
+    'no-else-return': 0,
+    'no-lonely-if': 0,
+    'space-unary-ops': 0,
+    'no-plusplus': 0,
+    'no-useless-constructor': 0,
   },
 };
