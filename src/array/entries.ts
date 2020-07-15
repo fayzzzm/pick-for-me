0da5f72afd4367d665b4c2ada6ad26248bda9552
fayzzz <mustafo.fayz@yandex.ru>
2020-07-15 13:40:47 +0500

    Added super puper type

--- src/array/entries.ts
+++ src/array/entries.ts
@@ -1,7 +1,24 @@
+const arr = [1, 2, ['s'], Promise, { a: 2, b: 3 }];
+
+type Prototype = Exclude<keyof typeof arr, keyof Array<never>>;
+type RecursionTypes<T> = T extends object
+    ? {
+          [K in keyof T]: K extends Prototype
+              ? 'never'
+              : T[K] extends Promise<infer Y>
+              ? T[K]
+              : T[K] extends (infer U)[]
+              ? RecursionTypes<U>
+              : T[K] extends object
+              ? RecursionTypes<T[K]>
+              : T[K];
+      }[keyof T]
+    : T;
+
 export const getEntries = <T>(
     item: T | T[],
     entries: string[] = [],
-): [string, any] => {
+): [string, RecursionTypes<T>] => {
     if (typeof item === 'object') {
         const _entries = Object.keys(item).reduce(
             (total: any[], key: string) => {
@@ -13,6 +30,9 @@ export const getEntries = <T>(
 
         return (_entries as any).flat();
     } else {
-        return [entries.map((entry) => `[${entry}]`).join(''), item];
+        return [
+            entries.map((entry) => `[${entry}]`).join(''),
+            item as RecursionTypes<T>,
+        ];
     }
 };
