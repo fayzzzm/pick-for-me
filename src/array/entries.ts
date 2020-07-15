e537d455b7297aaa46f412f3ea473d6f42b98f18
Mustafo Faiz <m.fayzov1@gmail.com>
2020-07-16 03:58:08 +0500

    🔨Refactored type, so now it works as it should do. (#19)

--- src/array/entries.ts
+++ src/array/entries.ts
@@ -1,16 +1,19 @@
-const arr = [1, 2, ['s'], Promise, { a: 2, b: 3 }];
-
-type Prototype = Exclude<keyof typeof arr, keyof Array<never>>;
-type RecursionTypes<T> = T extends object
+type RecursiveType<T> = T extends (infer Z)[]
     ? {
-          [K in keyof T]: K extends Prototype
-              ? 'never'
-              : T[K] extends Promise<infer Y>
-              ? T[K]
-              : T[K] extends (infer U)[]
-              ? RecursionTypes<U>
+          [K in keyof T]: T[K] extends infer Z
+              ? RecursiveType<Z>
               : T[K] extends object
-              ? RecursionTypes<T[K]>
+              ? RecursiveType<T[K]>
+              : T[K];
+      }[number]
+    : T extends Promise<infer D>
+    ? T
+    : T extends object
+    ? {
+          [K in keyof T]: T[K] extends (infer F)[]
+              ? RecursiveType<F>
+              : T[K] extends object
+              ? RecursiveType<T[K]>
               : T[K];
       }[keyof T]
     : T;
@@ -18,7 +21,7 @@ type RecursionTypes<T> = T extends object
 export const getEntries = <T>(
     item: T | T[],
     entries: string[] = [],
-): [string, RecursionTypes<T>] => {
+): [string, RecursiveType<T>] => {
     if (typeof item === 'object') {
         const _entries = Object.keys(item).reduce(
             (total: any[], key: string) => {
@@ -32,7 +35,7 @@ export const getEntries = <T>(
     } else {
         return [
             entries.map((entry) => `[${entry}]`).join(''),
-            item as RecursionTypes<T>,
+            item as RecursiveType<T>,
         ];
     }
 };
